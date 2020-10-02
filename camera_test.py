7dc879944bd5980f8b9a6e528bf35ef264d9fe15
mayank <sismmt09@gmail.com>
2020-10-03 04:51:54 +0530

    camera test

---
+++ camera_test.py
@@ -0,0 +1,12 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):          # to manual exit the frame.
+        break
+
+cap.release()
+cv2.destroyAllWindows()
