4daf8f64dfedebb6729fae3a1da16b410d4cd7c2
mayank <sismmt09@gmail.com>
2020-10-03 05:38:13 +0530

    added face detection in frame and highlight

--- faces.py
+++ faces.py
@@ -6,8 +6,19 @@ cap = cv2.VideoCapture(0)
 
 while 1:
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                                                      ############## gray holds the black and white version of frame image.           ############
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for(x,y,w,h) in faces:
+        print(x,y,w,h)
+
+    color = (255,0,0)  #BGR
+    stroke = 2
+    end_cord_y = y+h
+    end_cord_x = x+w
+    cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y), color, stroke)
+
     cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):          # to manual exit the frame.
+    if cv2.waitKey(20) & 0xFF == ord('q'):                                                          ############## to manual exit the frame.                                        ############
         break
 
 cap.release()
